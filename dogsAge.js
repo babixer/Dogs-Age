d4b8cc935b1ab69f21e73e3d9a9aa733ee9946fa
Babixer <51507608+babixer@users.noreply.github.com>
2020-02-27 20:54:02 +0100

    Add files via upload

---
+++ dogsAge.js
@@ -0,0 +1,9 @@
+var dogAge = prompt("Put your Dog's age");
+
+//Formula of dogs age
+
+var humanAge = (dogAge - 2) * 4 + 21;
+
+alert("Your dog's age is " + humanAge + " in humans age");
+
+// Try this code in your chrome's console 😎
